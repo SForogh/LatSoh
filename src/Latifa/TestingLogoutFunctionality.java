dd621a4f0f9087daf067360e2c51e06d4e6c5ed5
latifaforozan <la.forozan@gmail.com>
2022-12-22 17:46:33 -0800

    Test: QA123 add testing log out functionality

---
+++ src/Latifa/TestingLogoutFunctionality.java
@@ -0,0 +1,9 @@
+package Latifa;
+
+public class TestingLogoutFunctionality {
+
+    public static void main(String[] args) {
+
+        System.out.println("I am testing log out functionality");
+    }
+}
