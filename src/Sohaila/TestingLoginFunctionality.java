02aa4505f2f726c76b5607d94426ff9c56ecbb93
sforogh <sohaila.forogh@yahoo.com>
2022-12-22 17:46:41 -0800

    Test: qa1234 add testing of login functionality

---
+++ src/Sohaila/TestingLoginFunctionality.java
@@ -0,0 +1,10 @@
+package Sohaila;
+
+public class TestingLoginFunctionality {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("I ma testing login functionality in my company");
+    }
+}
